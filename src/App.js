321af1d9a4cdbf8385d6ba2eaceaa5b27fb035f3
choigaeul <choigaeul@suwon.ac.kr>
2025-11-08 02:04:14 +0900

    feat: card-list 컴포넌트 기본 레이아웃 및 스타일 구현

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
+=======
+import CardList from './Card_list/Card_list_ver1.jsx';
+import CardList2 from './Card_list/Card_list_ver2.jsx';
+>>>>>>> 1aca070 (feat: card-list 컴포넌트 기본 레이아웃 및 스타일 구현)
 import './App.css';
 import CardList from './Card_list/Card_list.jsx';
 
@@ -5,6 +10,10 @@ function App() {
   return (
     <>
       <CardList />
+<<<<<<< HEAD
+=======
+      <CardList2 />
+>>>>>>> 1aca070 (feat: card-list 컴포넌트 기본 레이아웃 및 스타일 구현)
     </>
   );
 }
