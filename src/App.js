94e27475001a7d86c55f35e102ba1a99caeaeaf4
Oh Young gyo <oey157@nate.com>
2025-11-07 13:37:01 +0900

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './Button/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-blue-500">
-          4팀 화이팅!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React or TailwindCSS
-        </a>
-      </header>
-    </div>
+    <>
+      <Button />
+    </>
   );
 }
 
