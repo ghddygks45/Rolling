6f14540394c2e63e50f3e8bf71a242ad1d62bb99
duddj122352-design <duddj122352@gmail.com>
2025-11-12 12:06:59 +0900

    feat: Header 태블릿 padding 적용

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -6,16 +6,19 @@ function Header() {
     <>
       <div className="border-b border-gray-200">
         <header className="bg-white sticky top-0 z-50 max-w-[1199px] h-[64px] mx-auto">
-          <div className="w-full h-full flex items-center justify-between">
+          {/* 
+            기본: 좌우 여백 없음
+            태블릿(md) 이상: 좌우 여백 px-[24px] 적용 
+          */}
+          <div className="w-full h-full flex items-center justify-between md:px-[24px]">
             {/* Rolling 로고 영역 */}
-            <div className="flex items-center justify-center justify-items-center z-60 h-[30px]">
-              {/* 로고 아이콘 */}
+            <div className="flex items-center justify-center z-60 h-[30px]">
               <a href="/">
                 <img src={RollingIcon} alt="Rolling 로고" />
               </a>
             </div>
 
-            {/* "롤링 페이퍼 만들기" 버튼 영역 */}
+            {/* "롤링 페이퍼 만들기" 버튼 */}
             <div>
               <button className="border border-grayscale-300 bg-white rounded-lg text-16-bold h-[42px] w-[149px] hover:bg-gray-100">
                 <a href="/">롤링 페이퍼 만들기</a>
